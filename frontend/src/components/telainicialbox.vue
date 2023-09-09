cbe28f2ec3335724541bb5497afdb51d7e43ea45
Ian Gabriel Braga Trinta <102569796+ianbraga30@users.noreply.github.com>
2023-09-09 16:16:54 -0300

    Add files via upload

--- frontend/src/components/telainicialbox.vue
+++ frontend/src/components/telainicialbox.vue
@@ -3,18 +3,33 @@
 
 <template>
     <div class="boxtelainicial">
-        <div class="subboxtelainicial">
+        <div >
             <div class="textbox">O gerente</div>
             <div class="smalltextbox">ficou completamente maluco!</div>
             <div>
                 <div class="evensmallertext">agradeçam à lenda Ian Gabriel Braga Trinta</div>
                 <div class="evensmallertext2">e ao seu fiel ajudante Luiz Mamals</div>
             </div>
+            
 
     </div>
         <div class="produto">
-            <img src="../components/lmao.png" alt="Descrição da imagem">
+        <!-- First line of products -->
+        <div class="product-row">
+          <img class="moldura" src="../components/lmao.png" alt="Descrição da imagem" width="200" height="200">
+          <img class="moldura" src="../components/fonts/whey.png" alt="Descrição da imagem" width="200" height="200">
+          
         </div>
+
+        <!-- Second line of products -->
+        <div class="product-row">
+          <img class="moldura" src="../components/pc gamer.png" alt="Descrição da imagem" width="200" height="200">
+          <img class="moldura" src="../components/one piece.png" alt="Descrição da imagem" width="200" height="200">
+          <span>
+            <img class="moldura" src="../components/pc.png" alt="Descrição da imagem" width="200" height="200">
+          </span>
+        </div>
+      </div>
     </div>
   </template>
   
@@ -32,8 +47,30 @@
   * {
     margin: 0;
     padding: 0;
-    
     font-family: 'ZZYZX', sans-serif;
+  }
+  .moldura {
+      border-radius: 10px;
+      border: 5px solid #fff; /* Add a white border */
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3); /* Add a shadow effect */
+      transition: transform 0.3s ease-in-out; /* Add a smooth transition */
+  }
+
+  .moldura:hover {
+      transform: scale(1.05); /* Scale up the image on hover for a cool effect */
+  }
+  .botao{
+    width: 50px;
+    background-color: #000000;
+    color: #ffffff;
+    position: fixed;
+    
+
+    z-index: 999;
+    display: flex;
+    
+    padding: 0 50px;
+    border-radius: 10px;
   }
   .produto{
     flex: 1; /* A imagem ocupa o espaço restante na caixa */
@@ -43,8 +80,41 @@
     padding-right: 20px; /* Espaçamento à direita da imagem */
     width: 80%;
   }
+  
     .subboxtelainicial{
         padding-left: 3%;
+    }
+    .textbox {
+        font-size: 34px;
+        color: #000000;
+        font-size: 65px;
+        font-weight: 600;
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        z-index: 999;
+        font-weight: bold;
+        margin-bottom: 0;
+        transition: all 0.6s ease-in-out;
+    }
+    .textbox:hover{
+      font-size: 75px;
+    }
+    .smalltextbox {
+        font-size: 28px;
+        color: #000000;
+        margin-top: 0;
+        font-size: 45px;
+        font-weight: 600;
+        z-index: 1000;
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        font-weight: bold;
+    }
+    .smalltextbox:hover{
+      font-size: 50px;
+      transition: all 0.6s ease-in-out;
     }
     .evensmallertext {
         font-size: 20px;
@@ -57,6 +127,14 @@
         font-weight: bold;
         margin-top: 3%;
     }
+    .evensmallertext:hover{
+      font-size: 20px;
+      transition: all 0.6s ease-in-out;
+    }
+    .evensmallertext2:hover{
+      font-size: 20px;
+      transition: all 0.6s ease-in-out;
+    }
     .evensmallertext2 {
         font-size: 1px;
         color: #888888;
@@ -67,43 +145,22 @@
         letter-spacing: 2px;
         font-weight: bold;
     }
-    .textbox {
-        font-size: 34px;
-        color: #000000;
-        font-size: 65px;
-        font-weight: 600;
-        text-decoration: none;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-weight: bold;
-        margin-bottom: 0;
-    }
-    .smalltextbox {
-        font-size: 28px;
-        color: #000000;
-        margin-top: 0;
-        font-size: 45px;
-        font-weight: 600;
-        text-decoration: none;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-weight: bold;
-    }
+    
   
   .boxtelainicial {
     width: 95%;
-    height: 70%;
-    background-color: #dbdbdb;
+    height: 700px;
+    background-color: #dcdcdc;
     color: #aa9a9a;
-    position: fixed;
-    top: 8vh;
-    left: 50%; 
+    position: relative;
+    top: 50px;
+    left: 50%;
     transform: translateX(-50%); /* Use transform to center horizontally */
-    z-index: 999;
+    z-index: 100;
     display: flex;
     align-items: center;
     padding: 0 50px;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    
     border-radius: 10px;
   }
   </style>

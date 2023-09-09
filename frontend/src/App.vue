fa88bd07b804b4a0fb1f5668998b0ceb30192bce
Ian Gabriel Braga Trinta <102569796+ianbraga30@users.noreply.github.com>
2023-09-09 16:14:30 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Navegacao from './components/navegacao.vue';
 import telainicialbox from './components/telainicialbox.vue';
+import equipeVue from './components/equipe.vue';  
+import produtosVue from './components/produtos.vue';
 </script>
 
 <template>
   <Navegacao />
-  <telainicialbox />
+  <telainicialbox></telainicialbox>
+  <produtosVue></produtosVue>
+  <equipeVue></equipeVue>
 </template>
 
 <style scoped>

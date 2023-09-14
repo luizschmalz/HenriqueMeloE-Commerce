206077d642ac42aee9ee2aa2eae71983bf7129bf
Ian Gabriel <igbt@cin.ufpe.br>
2023-09-13 21:03:29 -0300

    Had a small "typo" of a removed file, fixed it

--- frontend/src/views/loginadmparcerias.vue
+++ frontend/src/views/loginadmparcerias.vue
@@ -3,7 +3,6 @@ import Navegacao from '../components/navegacao.vue';
 import telainicialbox from '../components/telainicialbox.vue';
 import equipeVue from '../components/equipe.vue';  
 import produtosVue from '../components/produtos.vue';
-import Footer from '../components/footer.vue';
 </script>
 
 

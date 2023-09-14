b40068f92e6c3a5a084146e05b479c9c90159a76
Ian <underseadeer218@gmail.com>
2023-09-14 12:44:08 -0300

    testing does not work, Cin got no typescript/node

--- frontend/cypress/e2e/features/commom-step-definitions/addcupom.ts
+++ frontend/cypress/e2e/features/commom-step-definitions/addcupom.ts
@@ -1,5 +1,23 @@
-import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
-Given("o usuário está na página de {string}", (page:string) => {
-    cy.visit(page)
-});   
+Given('o usuário está na página de admparcerias', () => {
+  // You can set up your application's initial state here if needed
+  cy.visit('/admparcerias');
+});
+
+When('o usuário preenche o campo {string} com {string}', (field, value) => {
+  cy.get(`[data-cy=${field}]`).type(value);
+});
+
+When('o usuário preenche o campo {string} com {int}', (field, value) => {
+  cy.get(`[data-cy=${field}]`).type(value);
+});
+
+When('clica no botão {string}', (button) => {
+  cy.get(`[data-cy=${button}]`).click();
+});
+
+Then('o usuário é redirecionado para a página de parcerias', () => {
+  // Use Cypress commands to assert the redirection
+  cy.url().should('include', '/parcerias');
+});
